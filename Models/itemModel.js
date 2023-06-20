a57e59dccb35c9c1f306db2be67488f9b78b3507
Majaz <majazulhaque123@gmail.com>
2023-06-20 14:30:30 +0530

    Add validation to Item Model

--- Models/itemModel.js
+++ Models/itemModel.js
@@ -3,11 +3,15 @@ const mongoose = require('mongoose');
 const itemSchema = new mongoose.Schema({
     name:{
         type:String,
-        required:true
+        required:[true,"Please Enter Item Name"],
+        maxLength:[50,"Name cannot exceed 50 characters"],
+        minLength:[8,"Name should have more than 8 characters"],
     },
     description:{
         type:String,
-        required:true
+        required:[true,"Please Enter Item Name"],
+        maxLength:[500,"Description cannot exceed 500 characters"],
+        minLength:[50,"Description should have more than 50 characters"],
     }
 },{
     timestamps:true
