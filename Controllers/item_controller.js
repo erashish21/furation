b401dd9438f9bbcf0eaae9ddecdb2e3a8a157dc2
Majaz <majazulhaque123@gmail.com>
2023-06-20 11:24:14 +0530

    implement item create function in item_controller

--- Controllers/item_controller.js
+++ Controllers/item_controller.js
@@ -1,6 +1,23 @@
+const Item = require('../Models/itemModel');
+
+
 module.exports.create = async function(req,res){
-    res.status(200).json({
-        success:true,
-        message:"Run successfully"
-    });
-}
+    
+    try {
+        let item = await Item.create({
+            name:req.body.name,
+            description:req.body.description
+        });
+        
+        return res.status(200).json({
+            success:true,
+            message:"Item Created Successfully",
+            item
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:"Error while creating the item"
+        });
+    }
+};
