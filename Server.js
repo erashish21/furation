c330b7497246e36a23d6332c4b067df277c6514e
Majaz <majazulhaque123@gmail.com>
2023-06-20 10:45:30 +0530

    Setup Server.js and connect to server at port 4000

---
+++ Server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+
+//Config
+dotenv.config({path:"config/config.env"});
+
+const app = express();
+
+app.use(express.json());
+
+
+// Run the server
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port: ${process.env.PORT}`);
+});
